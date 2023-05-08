1e32fcd00b9517ace6aadf9828cc26903c0d7c9e
Niraj <nirajkm6664@gmail.com>
2023-05-08 17:36:49 +0530

    vertical issue fixed

--- assets/js/index.js
+++ assets/js/index.js
@@ -10,16 +10,23 @@
           {
             breakpoint: 1024,
             settings: {
-              slidesToShow: 3,
+              slidesToShow: 2,
               slidesToScroll: 3,
               infinite: true,
               dots: true
             }
           },
           {
+            breakpoint: 800,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 2
+            }
+          },
+          {
             breakpoint: 600,
             settings: {
-              slidesToShow: 2,
+              slidesToShow: 1,
               slidesToScroll: 2
             }
           },
